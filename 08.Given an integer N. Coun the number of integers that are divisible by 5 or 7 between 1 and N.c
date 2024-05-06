e646dca04e68f6e8f1cc85ad86265b2023a37ff4
Mr.Softy <123297701+MrSoftyXTrillion@users.noreply.github.com>
2024-05-06 19:12:19 +0600

    Create 08.Given an integer N. Coun the number of integers that are divisible by 5 or 7 between 1 and N.c

---
+++ 08.Given an integer N. Coun the number of integers that are divisible by 5 or 7 between 1 and N.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int i,n,count=0;
+    scanf ("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        if (i%5==0 || i%7==0)
+        {
+            count++;
+        }
+    }
+    printf ("%d\n",count);
+    return 0;
+}
