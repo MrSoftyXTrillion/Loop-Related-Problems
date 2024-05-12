9e4739cea13d39b2ba413da3024221b84d8483ba
Mr.Softy <123297701+MrSoftyXTrillion@users.noreply.github.com>
2024-05-13 02:17:58 +0600

    Create 78.Print Eight using astrick sign.c

---
+++ 78.Print Eight using astrick sign.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main ()
+{
+    int row,col,h,w;
+    scanf ("%d %d",&h,&w);
+    for (row=1;row<=h;row++)
+    {
+        if (row==1 || row==(h/2)+1 || row==h)
+        {
+            for (col=1;col<=w;col++)
+            {
+                printf ("*");
+            }
+            printf ("\n");
+        }
+        if (row>1 && row<(h/2)+1)
+        {
+            printf ("*");
+            for (col=2;col<=w-1;col++)
+            {
+                printf (" ");
+            }
+            printf ("*");
+            printf ("\n");
+        }
+        if (row>(h/2)+1 && row<h)
+        {
+            printf ("*");
+            for (col=2;col<=w-1;col++)
+            {
+                printf (" ");
+            }
+            printf ("*");
+            printf ("\n");
+        }
+    }
+    return 0;
+}
